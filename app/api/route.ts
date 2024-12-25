19ad8438a63f34bafaf340cc9db277b247d218b6
vathanak <vathanakkongsothy35@gmail.com>
2024-12-25 10:14:22 +0700

    Fix GET API response to return JSON and correct headers

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,9 +4,8 @@ import { NextResponse } from "next/server";
 export async function GET() {
     const user = await prisma.user.findMany();
     return new NextResponse(JSON.stringify(user), {
-        status: 405,
         headers: {
-            'Allow': 'POST',
+            "Content-Type": "application/json",
         },
     });
 }
